b83c967165a16885ccdbf9707e48e33336e20970
Aldo Escudero <ajescudero@gmail.com>
2024-06-03 10:43:15 -0300

    Add pundit authorization for books controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  # Ensure Pundit is used to handle authorization exceptions
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to(request.referrer || root_path)
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,25 @@ class BooksController < ApplicationController
   before_action :authorize_librarian!, except: %i[index show]
 
   def index
-    @books = Book.all
+    @books = policy_scope(Book)
   end
 
   def show
+    authorize @book
   end
 
   def new
     @book = Book.new
+    authorize @book
   end
 
   def edit
+    authorize @book
   end
 
   def create
     @book = Book.new(book_params)
+    authorize @book
     if @book.save
       redirect_to @book, notice: 'Book was successfully created.'
     else
@@ -27,6 +31,7 @@ class BooksController < ApplicationController
   end
 
   def update
+    authorize @book
     if @book.update(book_params)
       redirect_to @book, notice: 'Book was successfully updated.'
     else
@@ -35,12 +40,12 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    authorize @book
     @book.destroy
     redirect_to books_url, notice: 'Book was successfully destroyed.'
   end
 
   private
-
   def set_book
     @book = Book.find(params[:id])
   end
